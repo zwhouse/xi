7854e5074c1e187521120c50ab574eaab2cf5558
Bart Kiers <bkiers@gmail.com>
2018-09-26 10:16:06 +0200

    Generating ormconfig for production

---
+++ check-migrations.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [ -z "$DATABASE_URL" ]; then
+    # On a local dev machine, or on Travis CI, the DATABASE_URL will not be set.
+    echo "skipping 'typeorm migration:run'";
+else
+    IFS=':/@' read -ra TOKENS <<< "$DATABASE_URL"
+
+    username="${TOKENS[3]}";
+    password="${TOKENS[4]}";
+    host="${TOKENS[5]}";
+    port="${TOKENS[6]}";
+    database="${TOKENS[7]}";
+
+    echo "{
+           \"type\": \"postgres\",
+           \"host\": \"$host\",
+           \"port\": $port,
+           \"username\": \"$username\",
+           \"password\": \"$password\",
+           \"database\": \"$database\",
+           \"entities\": [\"dist/src/db/*.js\"],
+           \"migrationsTableName\": \"__migrations__\",
+           \"migrations\": [\"dist/src/migration/*.js\"],
+           \"cli\": {
+             \"migrationsDir\": \"src/migration\"
+           },
+           \"logging\": false
+         }" > "ormconfig-production.json";
+
+    typeorm migration:run --config ormconfig-production
+fi
