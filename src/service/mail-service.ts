ea2e8913aa925e3836f3d8ea9e41acd602c4324e
Bart Kiers <bkiers@gmail.com>
2018-10-09 20:01:02 +0200

    Fixed mail service

--- src/service/mail-service.ts
+++ src/service/mail-service.ts
@@ -5,22 +5,21 @@ export class MailService {
     async send(to: string, subject: string,  message: string) {
 
         if (!process.env.SENDGRID_API_KEY || process.env.SENDGRID_API_KEY === "TODO") {
-
-            sg.setApiKey(process.env.SENDGRID_API_KEY as string);
-
-            await sg.send({
-                from: "bkiers+xi@gmail.com",
-                to: to,
-                subject: subject,
-                text: message,
-                html: `<p>${message}</p>`
-            }).catch(reason => {
-                console.error(`couldn't send email: ${reason}`);
-            });
-        }
-        else {
             console.log("No SENDGRID_API_KEY set, not sending any email");
+            return;
         }
+
+        sg.setApiKey(process.env.SENDGRID_API_KEY as string);
+
+        await sg.send({
+            from: "bkiers+xi@gmail.com",
+            to: to,
+            subject: subject,
+            text: message,
+            html: `<p>${message}</p>`
+        }).catch(reason => {
+            console.error(`couldn't send email: ${reason}`);
+        });
     }
 }
 
